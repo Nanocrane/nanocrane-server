155086fb8bda895a00d35f8c08f3ec0214c338d3
Nanocrane <mail@martijnswinkels.nl>
2026-07-15 18:29:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,31 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+let muntKlaar = false;
+
 // Testpagina
 app.get("/", (req, res) => {
   res.send("Nanocrane server werkt!");
+});
+
+// Hiermee zetten we een munt klaar
+app.get("/betaald", (req, res) => {
+  muntKlaar = true;
+  console.log("Betaling ontvangen");
+  res.send("Munt klaargezet");
+});
+
+// ESP32 vraagt hier of er een munt is
+app.get("/munt", (req, res) => {
+
+  if (muntKlaar) {
+    muntKlaar = false;
+    console.log("Munt gegeven");
+    res.send("JA");
+  } else {
+    res.send("NEE");
+  }
+
 });
 
 app.listen(PORT, () => {
